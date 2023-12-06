a31e3faaf3866938c2197452e3f6fae58bd4b0d9
khosro akbari <ite.akbari@gmail.com>
2023-12-06 11:40:16 +0330

    edit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import CategoriesList from "@/components/Category/CategorisList";
+import IntroSwiper from "@/components/Swiper/IntroSwiper";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,14 +7,14 @@ export default function Home() {
   return (
     <div className="container lg:px-10 2xl:px-0 mx-auto">
       <main>
-        <div className="lg:px-26 py-20 h-max">
-          <div className="grid grid-cols-2">
-            <div className="">
+        <div className="lg:px-26 py-0 h-max">
+          <div className="grid grid-cols-1 lg:grid-cols-2 items-center">
+            <div className="order-2 lg:order-1">
               <div className="grid justify-items-center place-content-center">
-                <p className="text-center text-6xl font-extrabold leading-relaxed">
+                <p className="text-center text-3xl sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl font-extrabold leading-relaxed sm:leading-loose leading-md">
                   ما بهترین‌ کیفیت رو در <br /> ساری به دستتون می‌رسونیم.
                 </p>
-                <p className="text-center text-4xl font-bold py-5">
+                <p className="text-center text-2xl md:text-4xl font-bold py-5">
                   میوه‌های خاص با ترش‌وشیرین
                 </p>
                 <p className="text-center text-3xl">ارسال به سراسر ایران</p>
@@ -25,51 +26,39 @@ export default function Home() {
                 </Link>
               </div>
             </div>
-            <div className="flex justify-center">
+            <div className="flex justify-center xl:mt-16 mb-16 xl:mb-0 order-1 lg:order-2">
               <Image
                 src="/assets/img/ill.png"
                 width={300}
                 height={300}
                 alt="illustration"
-                className="w-4/6"
+                className="w-5/6 md:w-4/6"
               />
             </div>
           </div>
 
-          <div className="px-10 py-16 bg-light-green rounded-full flex justify-around my-24">
-            <div className="grid">
-              <Image
-                width={200}
-                height={200}
-                alt=""
-                src="/assets/img/f1.png"
-                className="w-36"
-              />
-              <p className="font-bold text-center text-4xl mt-4">تازگی</p>
-            </div>
-            <div className="grid">
-              <Image
-                width={200}
-                height={200}
-                alt=""
-                src="/assets/img/f2.png"
-                className="w-36"
-              />
-              <p className="font-bold text-center text-4xl mt-4">تنوع</p>
-            </div>
-            <div className="grid">
-              <Image
-                width={200}
-                height={200}
-                alt=""
-                src="/assets/img/basket.png"
-                className="w-36"
-              />
-              <p className="font-bold text-center text-4xl mt-4">خرید آسان</p>
-            </div>
+          <div className="px-10 sm:py-10 intro-swiper rounded-full  md:flex justify-around mt-16 mb-24">
+            <IntroSwiper />
           </div>
 
           <CategoriesList />
+
+          <div className="my-20 w-4/5 md:w-3/4 mx-auto pt-32">
+            <Image
+              src="/assets/img/banner.png"
+              width={400}
+              height={251}
+              alt="instagram banner"
+              className="w-full hidden md:block"
+            />
+            <Image
+              src="/assets/img/mbanner.png"
+              width={400}
+              height={251}
+              alt="instagram banner"
+              className="w-full md:hidden"
+            />
+          </div>
         </div>
       </main>
     </div>
---
+++ src/components/Swiper/IntroSwiper.jsx
@@ -0,0 +1,67 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay } from "swiper/modules";
+import Image from "next/image";
+
+const IntroSwiper = () => {
+  return (
+    <Swiper
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      breakpoints={{
+        650: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }}
+      modules={[Autoplay]}
+    >
+      <SwiperSlide>
+        <div className="grid justify-center">
+          <Image
+            width={200}
+            height={200}
+            alt=""
+            src="/assets/img/f1.png"
+            className="w-36"
+          />
+          <p className="font-bold text-center text-4xl mt-4">تازگی</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="grid justify-center">
+          <Image
+            width={200}
+            height={200}
+            alt=""
+            src="/assets/img/f2.png"
+            className="w-36"
+          />
+          <p className="font-bold text-center text-4xl mt-4">تنوع</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="grid justify-center">
+          <Image
+            width={200}
+            height={200}
+            alt=""
+            src="/assets/img/basket.png"
+            className="w-36"
+          />
+          <p className="font-bold text-center text-4xl mt-4">خرید آسان</p>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default IntroSwiper;
